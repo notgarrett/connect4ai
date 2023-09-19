0cfe5520fe0a5ee39d3bbd1d9c97aeecc0c3fbe6
notgarrett <garrettrgiles@gmail.com>
2023-09-18 23:49:06 -0400

    Init commit

---
+++ src/game.rs
@@ -0,0 +1,106 @@
+// Connect 4 sucks ass
+//
+
+const WIDTH: i32 = 7;
+const HEIGHT: i32 = 6;
+
+#[derive(Clone, Copy)]
+enum Cell {
+    Player1,
+    Player2,
+    Empty,
+}
+
+#[derive(Clone, Copy)]
+enum GameState {
+    Player1Win,
+    Player2Win,
+    Draw,
+    Ongoing,
+}
+
+#[derive(Clone)]
+enum Turns {
+    Player1Turn,
+    Player2Turn,
+}
+
+struct Game {
+    board: [Cell; (WIDTH * HEIGHT) as usize],
+    state: GameState,
+    turn: Turns,
+    count: usize,
+}
+
+impl Game {
+    fn new(&self) -> Self {
+        Self {
+            board: [Cell::Empty; (WIDTH * HEIGHT) as usize],
+            state: GameState::Ongoing,
+            turn: Turns::Player1Turn,
+            count: 0,
+        }
+    }
+
+    fn can_play(&self, col: usize) -> (bool, usize) {
+        if let GameState::Ongoing = self.state {
+            println!("Game is ongoing!");
+        } else {
+            return (false, 0);
+        }
+
+        for x in 0..5 {
+            if let Cell::Empty = self.board[col + (x * 7) as usize] {
+                return (true, col + (x * 7) as usize);
+            }
+        }
+
+        (false, 0)
+    }
+
+    fn play(&mut self, col: usize) -> GameState {
+        let (playable, position) = self.can_play(col);
+
+        if playable == false {
+            return GameState::Ongoing;
+        }
+
+        let current_turn = self.turn.clone();
+
+        match self.turn {
+            Turns::Player1Turn => {
+                self.turn = Turns::Player2Turn;
+                self.board[position] = Cell::Player1;
+            }
+            Turns::Player2Turn => {
+                self.turn = Turns::Player1Turn;
+                self.board[position] = Cell::Player2;
+            }
+        };
+
+        self.count += 1;
+
+        if self.check_win() {
+            match current_turn {
+                Turns::Player1Turn => self.state = GameState::Player1Win,
+                _ => self.state = GameState::Player2Win,
+            };
+        } else if self.count == 42 {
+            self.state = GameState::Draw;
+        }
+
+        self.state.clone()
+    }
+
+    fn is_winning_move(&self, col: usize) {
+        unimplemented!()
+    }
+
+    fn check_win(&self) -> bool {
+        unimplemented!()
+    }
+
+    fn check_win_horizontal(&self) -> bool {
+        unimplemented!()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod game;
+
+fn main() {
+    println!("Hello, world!");
+    let x: Vec<i32> = vec![];
+}
